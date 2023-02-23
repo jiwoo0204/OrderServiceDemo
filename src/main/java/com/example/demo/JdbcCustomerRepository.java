c1d5f3ab198ccf2857a688348f269bd9857f00a9
jiwoo0204 <sally12234@naver.com>
2023-02-23 11:45:24 +0900

    JDBC - logger message

--- src/main/java/com/example/demo/JdbcCustomerRepository.java
+++ src/main/java/com/example/demo/JdbcCustomerRepository.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.UUID;
 
 public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
@@ -18,7 +19,9 @@ public class JdbcCustomerRepository {
             resultSet = statement.executeQuery("select * from customers");
             while (resultSet.next()) {
                 var name = resultSet.getString("name");
-                logger.info("customer name -> {}", name);
+                var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
+                logger.info("customer id -> {}, name -> {}", customerId, name);
+
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
