4686d22e00a7e7b6d551146fe9940d039436f4b4
jiwoo0204 <sally12234@naver.com>
2023-02-23 11:29:55 +0900

    JDBC - logger

--- src/main/java/com/example/demo/JdbcCustomerRepository.java
+++ src/main/java/com/example/demo/JdbcCustomerRepository.java
@@ -7,12 +7,19 @@ import java.sql.*;
 
 public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
+
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
+        ResultSet resultSet = null;
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "lkj3591300!");
             statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from customers");
+            while (resultSet.next()) {
+                var name = resultSet.getString("name");
+                logger.info("customer name -> {}", name);
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
             logger.error("Got error while closing connection", throwables);
@@ -20,7 +27,8 @@ public class JdbcCustomerRepository {
             try {
                 if (connection != null) connection.close();
                 if (statement != null) statement.close();
-            } catch (SQLException exception){
+                if (resultSet != null) resultSet.close();
+            } catch (SQLException exception) {
                 logger.error("Got error while closing connection", exception);
             }
         }
