ba8e9be9189e7d92ef6ad1eb022eaae0cf7648e4
jiwoo0204 <sally12234@naver.com>
2023-02-23 11:52:15 +0900

    JDBC - autoclose

--- src/main/java/com/example/demo/JdbcCustomerRepository.java
+++ src/main/java/com/example/demo/JdbcCustomerRepository.java
@@ -10,30 +10,23 @@ public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
 
     public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "lkj3591300!");
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from customers");
+//        Connection connection = null;
+//        Statement statement = null;
+//        ResultSet resultSet = null;
+        try (
+            var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "lkj3591300!");
+            var statement = connection.createStatement();
+            var resultSet = statement.executeQuery("select * from customers");
+
+        ) {
             while (resultSet.next()) {
                 var name = resultSet.getString("name");
                 var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
                 logger.info("customer id -> {}, name -> {}", customerId, name);
-
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        }
+        catch (SQLException throwables) {
             logger.error("Got error while closing connection", throwables);
-        } finally {
-            try {
-                if (connection != null) connection.close();
-                if (statement != null) statement.close();
-                if (resultSet != null) resultSet.close();
-            } catch (SQLException exception) {
-                logger.error("Got error while closing connection", exception);
-            }
         }
     }
 }
