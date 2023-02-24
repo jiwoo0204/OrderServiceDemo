0b5044cfb4d43fec034ea8548f2c341ee5e52c0d
jiwoo0204 <sally12234@naver.com>
2023-02-24 10:25:19 +0900

    JDBC - prepare statement

--- src/main/java/com/example/demo/JdbcCustomerRepository.java
+++ src/main/java/com/example/demo/JdbcCustomerRepository.java
@@ -13,24 +13,25 @@ public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
 
     public List<String> findNames(String name) {
-        var SELECT_SQL = MessageFormat.format("select * from customers WHERE name = ''{0}''", name);
+        var SELECT_SQL = "select * from customers WHERE name = ?";
         List<String> names = new ArrayList<>();
         try (
                 var connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "lkj3591300!");
-                var statement = connection.createStatement();
-                var resultSet = statement.executeQuery(SELECT_SQL)
-
+                var statement = connection.prepareStatement(SELECT_SQL)
         ) {
-            while (resultSet.next()) {
-                var customerName = resultSet.getString("name");
-                var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
-                var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
-                logger.info("customer id -> {}, name -> {}, createdAt -> {}", customerId, customerName, createdAt);
-                names.add(customerName);
+            statement.setString(1, name);
+            try (var resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    var customerName = resultSet.getString("name");
+                    var customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
+                    var createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
+                    logger.info("customer id -> {}, name -> {}, createdAt -> {}", customerId, customerName, createdAt);
+                    names.add(customerName);
+                }
             }
         }
-        catch (SQLException throwables) {
-            logger.error("Got error while closing connection", throwables);
+        catch (SQLException throwable) {
+            logger.error("Got error while closing connection", throwable);
         }
 
         return names;
